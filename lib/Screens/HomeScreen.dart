62131afdaa95003ada769395bdb3b43a286a55d0
muhammedrahil <mrahil7510@gmail.com>
2021-12-25 10:49:43 +0530

    video icon added

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -90,11 +90,9 @@ class HomeScreen extends StatelessWidget {
               //       fontSize: 45.6, fontWeight: FontWeight.w700),
               // ),
             ),
-
             const SizedBox(
               width: 10,
             ),
-
             SizedBox(
               height: 250,
               child: ListView(
@@ -121,9 +119,8 @@ class HomeScreen extends StatelessWidget {
                 ],
               ),
             ),
-
-                Padding(
-              padding: EdgeInsets.only(left: 28.8, top: 28.8,bottom: 16),
+            Padding(
+              padding: EdgeInsets.only(left: 28.8, top: 28.8, bottom: 16),
               child: Row(
                 children: [
                   Text(
@@ -136,126 +133,106 @@ class HomeScreen extends StatelessWidget {
                 ],
               ),
             ),
-             Padding(
-               padding: const EdgeInsets.all(8.0),
-               child: const SizedBox(
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: const SizedBox(
                 width: 10,
+              ),
             ),
-             ),
-    SizedBox(
+            SizedBox(
               height: 45.6,
               child: Padding(
                 padding: const EdgeInsets.only(left: 25),
                 child: ListView(
                   scrollDirection: Axis.horizontal,
-                  
                   children: [
-                      IconsCard(
-                            iconData: 'hello',
-                            icons: Icons.home,
-                            color: Colors.blue[200],
-                            Textcolor: Colors.blue,
-                            onTap: () {
-                              print('hell');
-                            }),
-                             Padding(padding: EdgeInsets.all(1)),
-                             IconsCard(
-                            iconData: 'hello',
-                            icons: Icons.search,
-                            color: Colors.brown[200],
-                            Textcolor: Colors.brown,
-                            onTap: () {
-                              print('hello');
-                            }),
-                        Padding(padding: EdgeInsets.all(1)),
-                        IconsCard(
-                            iconData: 'hello',
-                            icons: Icons.home,
-                            color: Colors.green[200],
-                            Textcolor: Colors.green,
-                            onTap: () {
-                              print('object');
-                            }),
+                    IconsCard(
+                        iconData: 'hello',
+                        icons: Icons.home,
+                        color: Colors.blue[200],
+                        Textcolor: Colors.blue,
+                        onTap: () {
+                          print('hell');
+                        }),
+                    Padding(padding: EdgeInsets.all(1)),
+                    IconsCard(
+                        iconData: 'hello',
+                        icons: Icons.search,
+                        color: Colors.brown[200],
+                        Textcolor: Colors.brown,
+                        onTap: () {
+                          print('hello');
+                        }),
+                    Padding(padding: EdgeInsets.all(1)),
+                    IconsCard(
+                        iconData: 'hello',
+                        icons: Icons.home,
+                        color: Colors.green[200],
+                        Textcolor: Colors.green,
+                        onTap: () {
+                          print('object');
+                        }),
                   ],
                 ),
               ),
             ),
- 
-    Padding(
-               padding: const EdgeInsets.all(8.0),
-               child: const SizedBox(
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: const SizedBox(
                 width: 10,
+              ),
             ),
-             ),
-            
-                 SizedBox(
+            SizedBox(
               height: 150,
               child: ListView(
                 scrollDirection: Axis.horizontal,
-                
                 children: [
-                   SmallCard(
-                    NextPageAddress: () {
-                      Navigator.of(context)
-                          .push(MaterialPageRoute(builder: (ctx) {
-                        return const Content_Screen();
-                      }));
-                    },
-                    ImagePath: 'assets/fall18.png',
-                    TextColors: Colors.black87,
+                  SmallCard(
+                      NextPageAddress: () {
+                        Navigator.of(context)
+                            .push(MaterialPageRoute(builder: (ctx) {
+                          return const Content_Screen();
+                        }));
+                      },
+                      ImagePath: 'assets/fall18.png',
+                      TextColors: Colors.black87,
                       TextDiscription: 'TextDiscriptionTextDiscription'),
-                      
-                SmallCard(
-                    NextPageAddress: () {
-                      Navigator.of(context)
-                          .push(MaterialPageRoute(builder: (ctx) {
-                        return const Content_Screen();
-                      }));
-                    },
-                    ImagePath: 'assets/fall8.png',
-                    TextColors: Colors.white,
-                    TextDiscription: 'TextDiscriptionTextDiscription'),
+                  VedioCard(
+                      NextVideoAddress: () {},
+                      TextDiscription: '',
+                      TextColors: Colors.white,
+                      ImagePath: 'assets/fall4.png')
                 ],
               ),
             ),
-        
-       
-
-                 SizedBox(
+            SizedBox(
               height: 150,
               child: ListView(
                 scrollDirection: Axis.horizontal,
-                
                 children: [
-                 
-
-                     SmallCard(
-                    NextPageAddress: () {
-                      Navigator.of(context)
-                          .push(MaterialPageRoute(builder: (ctx) {
-                        return const Content_Screen();
-                      }));
-                    },
-                    ImagePath: 'assets/fall4.png',
-                    TextColors: Colors.white,
-                    TextDiscription: 'TextDiscriptionTextDiscription'),
-                SmallCard(
-                    NextPageAddress: () {
-                      Navigator.of(context)
-                          .push(MaterialPageRoute(builder: (ctx) {
-                        return const Content_Screen();
-                      }));
-                    },
-                    ImagePath: 'assets/fall1.png',
-                    TextColors: Colors.black87,
-                    TextDiscription: 'TextDiscriptionTextDiscription'),
+                  SmallCard(
+                      NextPageAddress: () {
+                        Navigator.of(context)
+                            .push(MaterialPageRoute(builder: (ctx) {
+                          return const Content_Screen();
+                        }));
+                      },
+                      ImagePath: 'assets/fall4.png',
+                      TextColors: Colors.white,
+                      TextDiscription: 'TextDiscriptionTextDiscription'),
+                  SmallCard(
+                      NextPageAddress: () {
+                        Navigator.of(context)
+                            .push(MaterialPageRoute(builder: (ctx) {
+                          return const Content_Screen();
+                        }));
+                      },
+                      ImagePath: 'assets/fall1.png',
+                      TextColors: Colors.black87,
+                      TextDiscription: 'TextDiscriptionTextDiscription'),
                 ],
               ),
             ),
-        
-
-            
-        
           ],
         ),
       ),
@@ -329,6 +306,41 @@ Widget SmallCard({
           borderRadius: BorderRadius.circular(15.5)),
       child: Stack(
         children: [
+          Positioned(
+            bottom: 10,
+            left: 10,
+            child: Text(
+              TextDiscription,
+              style: TextStyle(color: TextColors, fontSize: 10),
+            ),
+          )
+        ],
+      ),
+    ),
+  );
+}
+
+Widget VedioCard({
+  required void Function() NextVideoAddress,
+  required String TextDiscription,
+  required Color TextColors,
+  required String ImagePath,
+}) {
+  return ElevatedButton(
+    onPressed: NextVideoAddress,
+    style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(Colors.white),
+        elevation: MaterialStateProperty.all(0)),
+    child: Container(
+      height: 124,
+      width: 155,
+      decoration: BoxDecoration(
+          image:
+              DecorationImage(image: AssetImage(ImagePath), fit: BoxFit.cover),
+          borderRadius: BorderRadius.circular(15.5)),
+      child: Stack(
+        children: [
+          Positioned(left: 55,top: 35, child: Icon(Icons.video_collection,size: 50,)),
           Positioned(
             bottom: 10,
             left: 10,
