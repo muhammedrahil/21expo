49af7fd3fd0f28ea306bbd25360601d8c85ad7b1
mrkaalv <81458861+mrkaalv@users.noreply.github.com>
2021-12-26 10:24:23 +0530

    contents added texts.dart

--- lib/textFolder/texts.dart
+++ lib/textFolder/texts.dart
@@ -1,27 +1,73 @@
 
 
 // ignore: non_constant_identifier_names
-String History = '''How to Publish a Short Story: Submit Your Story
+String History = '''Malabar Rebellion History
 
-How to Publish a Short Story: Final Steps. Well, we’re here. You’ve planned, written, and workshopped your short story. It’s ready to go. Now it’s time to release that baby into the world! This is the scary part. You’re looking at the guidelines for the publication you chose week one and you think your story is ready. But how exactly do you go about sending one out? What all do you need? What’s the professional etiquette? Here are the last steps to submission: 10. Prepare Your ...
+The Malabar rebellion happened from August 20, 1921 to 1922 in the Malabar region of Kerala, India. 
+The Malabar rebellion of 1921 (also known by the names Moplah massacre, Moplah riots, Mappila riots) started as a resistance against
+ the British colonial rule in Malabar region of Kerala. The popular uprising was also against the prevailing feudal system controlled by elite Hindus.
+The British had appointed high caste Hindus in positions of authority to get their support, this led to the protest turning against the Hindus.
+For many scholars, the rebellion is primarily a peasant revolt against the colonial government.During the uprising, the rebels also attacked 
+various symbols and institutions of the colonial state, such as telegraph lines, train stations, courts and post offices.
+The main leaders of the rebellion were Ali Musliyar, Variankunnath Kunjahammad Haji, Sithi Koya Thangal, M. P. Narayana Menon,
+Chembrasery Thangal, K. Moideenkutti Haji, Kappad Krishnan Nair, Konnara Thangal, Pandiyatt Narayanan Nambeesan,
+and Mozhikunnath Brahmadathan Nambudiripad.
 
-Where to Publish Writing by Teens & Older Students
-stonesoup.com
-First published in 1992, The Claremont Review published short stories, poems, short plays, graphic art, photography, and interviews each year in the spring and fall, although no fall 2018 or spring 2019 issues are planned. Visit their facebook page for updates on this publication’s current status. Website. Launch Pad. Age Range: 6 to 14 Accepts: fiction, nonfiction, and poetry, and illustrations by assignment. Launch Pad publishes stories, art, poetry, nonfiction, and book reviews by kids ...
+There were a series of clashes between the Mappila peasantry and their landlords, supported by the colonial government,
+ throughout the 19th and early 20th centuries. The heavy-handed suppression of the Khilafat Movement by the colonial 
+government was met by resistance in the Eranad and Valluvanad taluks of Malabar. In the initial stages, the movement had 
+the support of Mohandas Gandhi and other Indian nationalist leaders, and a number of clashes took place between Khilafat
+ volunteers and other religious communities, but the violence soon spread across the region.The Mappilas attacked and 
+took control of police stations, colonial government offices, courts and government treasuries. Though the crowds who
+ attacked and destroyed the public offices in Eranad, Valluvanad, and Ponnani Taluks were Mappilas, sometimes Nambudiris,
+ Nairs and Thiyyas also acted as leaders in many parts of Valluvanad. British historians, on the other hand, referred to the rebellion 
+as the Mappila revolt.
 
-Wattpad - Where stories live
-www.wattpad.com
-Wattpad Books aspires to recognize and reflect diverse voices by taking Wattpad stories to published book and onto bookshelves around the world. Wattpad Books works with partners such as: Find out more about what we do for writers → How Wattpad Works. Get your story discovered through the power of community and technology on Wattpad. 1. Create. Share your''';
+The colonial government took a heavy-handed approach towards the rebellion. Colonial troops were sent to the area and martial law imposed.
+ One of the most noteworthy events during the rebellion later came to be known as the "Wagon Tragedy", in which 67 out of a total of 90
+ Mappila prisoners destined for the Central Prison in Podanur suffocated in a closed railway goods wagon. For six months from August 1921,
+ the rebellion extended over 2,000 square miles (5,200 km2) – some 40% of the South Malabar region of the Madras Presidency.
+ An estimated 10,000 people lost their lives, although official figures put the numbers at 2337 rebels killed, 1652 injured and 45,404 imprisoned.
+ Unofficial estimates put the number imprisoned at almost 50,000 of whom 20,000 were deported, mainly to the penal colony in the Andaman Islands, 
+while around 10,000 went missing.Sumit Sarkar in Modern India quotes an Arya Samaj source that claimed about 600 Hindus were killed and 2,500
+ forcibly converted during the rebellion.Variamkunnath Kunnahammad Haji claimed to have killed the alleged British agents and spies responsible 
+for the forced conversion of Hindus to Mohammadanism and killing others. Theprominent leaders of the rebellion were Variankunnath Kunjahammad Haji,
+ Sithi Koya Thangal and Ali Musliyar.
+
+Contemporary colonial administrators and modern historians differ markedly in their assessment of the incident, debating whether the revolts were triggered 
+off by religious fanaticism or agrarian grievances. At the time, the Indian National Congress repudiated the movement and it remained isolated from the 
+wider nationalist movement. However, some contemporary Indian evaluations now view the rebellion as a national upheaval against colonial rule and the 
+most important event concerning the political movement in Malabar during the period.
+
+In its magnitude and extent, it was an unprecedented popular upheaval, the likes of which has not been seen in Kerala before or since. 
+While the Mappilas were in the vanguard of the movement and bore the brunt of the struggle, several non-Mappila leaders actively sympathised with the rebels' cause, 
+giving the uprising the character of a national upheaval. In 1971, the Government of Kerala officially recognised the active participants in the events as freedom fighters.''';
 
 // ignore: non_constant_identifier_names
-String Article = '''How to Publish a Short Story: Submit Your Story
+String Ali Musliyar = '''Ali Musliyar
 
-How to Publish a Short Story: Final Steps. Well, we’re here. You’ve planned, written, and workshopped your short story. It’s ready to go. Now it’s time to release that baby into the world! This is the scary part. You’re looking at the guidelines for the publication you chose week one and you think your story is ready. But how exactly do you go about sending one out? What all do you need? What’s the professional etiquette? Here are the last steps to submission: 10. Prepare Your ...
+Āli Musliyār (born Erikkunnan Pālattu Mūlayil Āli; 1861–1922) was one of the leaders of the Malabar Rebellion.
+ Musliyār was the Imam of Tirurangadi Masjid from 1907 until his eventual execution at Coimbatore Prison for leading the Malabar Rebellion. 
+He was an active orator of the Khilafat Movement. In 2021, Indian Council of Historical Research gave recommendation to the Indian Government 
+to remove the names of Haji and 386 others from the Dictionary of Martyrs of India’s Freedom Struggle.
 
-Where to Publish Writing by Teens & Older Students
-stonesoup.com
-First published in 1992, The Claremont Review published short stories, poems, short plays, graphic art, photography, and interviews each year in the spring and fall, although no fall 2018 or spring 2019 issues are planned. Visit their facebook page for updates on this publication’s current status. Website. Launch Pad. Age Range: 6 to 14 Accepts: fiction, nonfiction, and poetry, and illustrations by assignment. Launch Pad publishes stories, art, poetry, nonfiction, and book reviews by kids ...
 
-Wattpad - Where stories live
-www.wattpad.com
-Wattpad Books aspires to recognize and reflect diverse voices by taking Wattpad stories to published book and onto bookshelves around the world. Wattpad Books works with partners such as: Find out more about what we do for writers → How Wattpad Works. Get your story discovered through the power of community and technology on Wattpad. 1. Create. Share your''';
+Āli Musliyar was born in Nellikkunattu desom, Eranad taluk, Malabar district to Kunhimoitīn Molla and Kōtakkal Āmina.
+ Kōtakkal Āmina was a member of the famous Maqdoom family of Ponnani, known for their extreme religious.
+ Musliyar's grandfather, Mūsa, was one of several "Malappuram Martyrs". Ali Musliyar began his education studying the Qur'an
+, tajwīd and the Malayālam language with Kakkadammal Kunnukammu Molla. He was sent to Ponnani Darse for further studies in religion and philosophy,
+ under the tutelage of Sheikh Zainuddin Maqdum I (Akhir), which he successfully completed after 10 years.[citation needed]
+
+He then went to Haram, Mecca for further education. Throughout this period, he was guided by several famous scholars,
+ including Sayyid Ahmed Sahni Dahlan, Shiekh Muhammed Hisbullahi Makki, and Sayyid Husain Habshi. After spending seven years in Mecca
+ he went on to serve as the Chief Qasi in Kavaratti, Laccadive Islands.''';
+
+// ignore: non_constant_identifier_names
+String Variyan Kunnathu = '''Variyan Kunnathu Kunjahammed Haji
+
+
+Variyankunnathu Kunjahammed Haji also called Variamkunnan (1883 – 20 January 1922) was an active member of Khilafat Movement.
+According to RH Hitchcock who took part in the fight against the rebellion "Khilaft to him (Variamkunnath Kunjahammad Haji) was a Turkish matter not Indian".
+ He was an ordinary member of Khilafat Movement.He established a parallel government in Malabar District of Madras Presidency in open defiance of the British Raj, 
+which lasted for a short period of six months. With his long term mentor, Ali Musliyar, Kunjahammed Haji seized large areas of the then Eranad and Valluvanad taluks, 
+now part of the Kerala state in India from British control.He said he was fighting for the independence of India''';
